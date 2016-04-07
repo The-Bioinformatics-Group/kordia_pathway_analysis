f4d7cde8f1332bf2cc7e9492911e4fd64c4e8e64
Oskar Vidarsson <oskarvidarsson@gmail.com>
2016-04-07 17:51:19 +0200

    Added "FASTA_files/" to each FASTA file path

--- reversecomplement.r
+++ reversecomplement.r
@@ -6,17 +6,17 @@ library(Biostrings)
 
 setwd("~/kordia_pathway_analysis/")
 
-dna1<-readDNAStringSet("seq1_16536_16552_circcq_fixed.fasta")
+dna1<-readDNAStringSet("FASTA_files/seq1_16536_16552_circcq_fixed.fasta")
 
 dnarev<-reverseComplement(dna1)
 
-writeXStringSet(dnarev,"unitig1revcomp.fasta",append=FALSE)
+writeXStringSet(dnarev,"FASTA_files/unitig1revcomp.fasta",append=FALSE)
 
-dna2 <- readDNAStringSet("unitig_2_circ_asof_may_2015.fasta")
+dna2 <- readDNAStringSet("FASTA_files/unitig_2_circ_asof_may_2015.fasta")
 
-writeXStringSet(dnarev,"combined-revcomp-unitig-1-2.fasta",append=FALSE)
+writeXStringSet(dnarev,"FASTA_files/combined-revcomp-unitig-1-2.fasta",append=FALSE)
 
 ## You need to manually add ~100 N at the end of combined-unitig-1-2.fasta before you proceed and
 ## the next DNA sequence to be appended still has ">1|quiver" on top of it, you need to manually remove it.
 
-writeXStringSet(dna2,"combined-revcomp-unitig-1-2.fasta",append=TRUE)
+writeXStringSet(dna2,"FASTA_files/combined-revcomp-unitig-1-2.fasta",append=TRUE)
